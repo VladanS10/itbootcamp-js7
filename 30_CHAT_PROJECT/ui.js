4922777e7cea056ea179d21cc6a5ccc05de69a82
VladanS10 <vladan.nish@gmail.com>
2022-02-01 16:23:28 +0100

    TASK_9 Sredjivanje vremena

--- 30_CHAT_PROJECT/ui.js
+++ 30_CHAT_PROJECT/ui.js
@@ -11,13 +11,34 @@ class ChatUl {
         return this._lista
     }
 
+    formatDate(date){
+        let d = date.getDate();
+        let m = date.getMonth() + 1;
+        let y = date.getFullYear();
+        let h = date.getHours();
+        let min = date.getMinutes();
+
+        d = String(d).padStart(2, "0")
+        m = String(m).padStart(2, "0")
+        h = String(h).padStart(2, "0")
+        min = String(min).padStart(2, "0")
+
+        let strDate = d + "." + m + "." + y + ". - " + h + ":" + min;
+
+        return strDate;
+    }
+
     templateLi(data){
-        
+        let date = data.created_at.toDate();
+        let strDate = this.formatDate(date);
+
         let listHtml =
         `<li>
             ${data.username} : ${data.message}
             <br>
-            ${data.created_at}
+            ${strDate}
+            
+            
         </li>
         `
         this.lista.innerHTML += listHtml
